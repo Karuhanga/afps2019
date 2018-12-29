46da080d22b0382bb8376e15f321bb140ed4cf4e
karuhanga <karuhanga475@gmail.com>
2018-12-29 03:18:47 +0300

    Fixed static file not found issue

--- presence/settings/base.py
+++ presence/settings/base.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -113,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../../visible/static")
+    os.path.join(BASE_DIR, "../visible/static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "../../staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "../staticfiles")
