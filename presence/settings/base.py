fed602124cdeb2b31d3aa416d2cac66e3358cb4b
karuhanga <karuhanga475@gmail.com>
2018-12-29 03:11:06 +0300

    Fixed static file not found issue

--- presence/settings/base.py
+++ presence/settings/base.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "visible/../../visible/static")
+    os.path.join(BASE_DIR, "../visible/static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../../staticfiles")
